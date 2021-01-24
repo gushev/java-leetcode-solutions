4be379066347b905c760bd6662efff681d320821
Georgi Ushev <ushev.georgi@gmail.com>
2021-01-24 12:00:02 +0200

    Add leetcode/romantointeger

--- src/leetcode/romantointeger/README.md
+++ src/leetcode/romantointeger/README.md
@@ -1,3 +1,5 @@
-# 7. [Reverse Integer](https://leetcode.com/problems/reverse-integer/)
+# 13. [Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
 
-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Given a roman numeral, convert it to an integer.
--- src/leetcode/romantointeger/Solution.java
+++ src/leetcode/romantointeger/Solution.java
@@ -1,2 +1,40 @@
-package leetcode.romantointeger;public class Solution {
+package leetcode.romantointeger;
+
+class Solution {
+  public int romanToInt(String s) {
+    int result = 0;
+
+    for(int i = 0; i < s.length(); i++) {
+      char previousChar = s.charAt(i);
+      char nextChar = 'a';
+      if (i + 1 < s.length()) {
+        nextChar = s.charAt(i + 1);
+      }
+
+      int currentNumber = getValue(s.charAt(i));
+
+      if (previousChar == 'I' && (nextChar == 'V' || nextChar == 'X') ||
+        previousChar == 'X' && (nextChar == 'L' || nextChar == 'C') ||
+        previousChar == 'C' && (nextChar == 'D' || nextChar == 'M')) {
+        currentNumber = -currentNumber;
+      }
+
+      result += currentNumber;
+    }
+
+    return result;
+  }
+
+  private int getValue(char c){
+    return switch (c) {
+      case 'I' -> 1;
+      case 'V' -> 5;
+      case 'X' -> 10;
+      case 'L' -> 50;
+      case 'C' -> 100;
+      case 'D' -> 500;
+      case 'M' -> 1000;
+      default -> 0;
+    };
+  }
 }
