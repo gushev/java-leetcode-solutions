021441fa6afa89e69eeafb160d8a3578361f0819
Georgi Ushev <ushev.georgi@gmail.com>
2021-01-27 00:13:59 +0200

    Add jumping frog problem - simple

--- src/other/jumpingfrog/Main.java
+++ src/other/jumpingfrog/Main.java
@@ -1,2 +1,12 @@
-package other.jumpingfrog;public class Main {
+package other.jumpingfrog;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(jumpFeet(4));
+    }
+
+    public static int jumpFeet(int feet) {
+        if (feet <= 2) return feet;
+        return jumpFeet(feet - 1) + jumpFeet(feet - 2);
+    }
 }
