def8dda811ea4187caaaac2613194d81dc877192
Georgi Ushev <ushev.georgi@gmail.com>
2021-01-26 11:05:50 +0200

    Add factorial recursive function

---
+++ src/other/factorial/Main.java
@@ -0,0 +1,12 @@
+package other.factorial;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(fact(10));
+  }
+
+  private static long fact(int n) {
+    if (n <= 1) return 1;
+    return n * fact(n - 1);
+  }
+}
