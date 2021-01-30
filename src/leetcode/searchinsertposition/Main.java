9fcd7516d86a2a878d2fe74074e1103cd92e81e1
Georgi Ushev <ushev.georgi@gmail.com>
2021-01-30 12:42:04 +0200

    Add leetcode/searchinsertposition

---
+++ src/leetcode/searchinsertposition/Main.java
@@ -0,0 +1,12 @@
+package leetcode.searchinsertposition;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 5));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 2));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 7));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 0));
+        System.out.println(solution.searchInsert(new int[] {1}, 0));
+    }
+}
