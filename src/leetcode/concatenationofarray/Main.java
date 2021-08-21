ef6f8b5bbd29ad06b677ee588d653cf566d387dc
Georgi Ushev <ushev.georgi@gmail.com>
2021-08-21 14:56:36 +0300

    Concatenation of Array solution

---
+++ src/leetcode/concatenationofarray/Main.java
@@ -0,0 +1,10 @@
+package leetcode.concatenationofarray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.getConcatenation(new int[]{1,3,2,1})));
+    }
+}
