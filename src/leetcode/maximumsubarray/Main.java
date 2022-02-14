49e8f0f91fa2b9eebf67ffad8b1e1017e586e770
Georgi Ushev <ushev.georgi@gmail.com>
2022-02-14 11:01:59 +0200

    Maximum subarray solution

--- src/leetcode/maximumsubarray/Main.java
+++ src/leetcode/maximumsubarray/Main.java
@@ -3,6 +3,6 @@ package leetcode.maximumsubarray;
 public class Main {
   public static void main(String[] args) {
     Solution solution = new Solution();
-    System.out.println(solution.maxSubArray(new int[]{1, 2, 3}));
+    System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
   }
 }
--- src/leetcode/maximumsubarray/Solution.java
+++ src/leetcode/maximumsubarray/Solution.java
@@ -3,7 +3,16 @@ package leetcode.maximumsubarray;
 class Solution {
   public int maxSubArray(int[] nums) {
 
-    return nums[0];
+    int[] memo = new int[nums.length];
+    memo[0] = nums[0];
+    int max = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      memo[i] = nums[i] + (Math.max(memo[i - 1], 0));
+      max = Math.max(memo[i], max);
+    }
+
+    return max;
   }
 
 }
