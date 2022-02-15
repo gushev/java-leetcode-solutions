8b66516b7c9994b83a8214fcf87e550ce56becb4
Georgi Ushev <ushev.georgi@gmail.com>
2022-02-15 10:15:19 +0200

    Solve leetcode - Length of Last Word

---
+++ src/leetcode/lengthoflastword/Main.java
@@ -0,0 +1,8 @@
+package leetcode.lengthoflastword;
+
+public class Main {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.lengthOfLastWord("   fly me   to   the moon  "));
+  }
+}
