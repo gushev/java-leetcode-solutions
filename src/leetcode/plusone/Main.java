742e1b2cc69644d9f34a62ad2ccb30316a7891a5
Georgi Ushev <ushev.georgi@gmail.com>
2022-03-06 22:12:16 +0200

    Leetcode Plusone Solution

---
+++ src/leetcode/plusone/Main.java
@@ -0,0 +1,10 @@
+package leetcode.plusone;
+
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(Arrays.toString(solution.plusOne(new int[]{8, 9, 9, 9})));
+  }
+}
