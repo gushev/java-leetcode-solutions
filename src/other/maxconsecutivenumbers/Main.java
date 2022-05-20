a3e0270187ff06776411a8b87b5121d372c657bf
Georgi Ushev <ushev.georgi@gmail.com>
2022-05-20 22:20:11 +0300

    Improve solution

--- src/other/maxconsecutivenumbers/Main.java
+++ src/other/maxconsecutivenumbers/Main.java
@@ -4,7 +4,16 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] input = new int[]{1};
-    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers(input)));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{1, 2, 3, 4, 5, 5, 7, 8, 8, 4, 2, 2, 2, 1}))));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{1}))));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{1, 2}))));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{-1, 2, -2, -2}))));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{0, 0, 0, -2, -2}))));
+    System.out.println(Arrays.toString(Solution.findMaxDuplicateNumbers((new int[]{1, 1, 1, 3, -2, -2}))));
+    try {
+      Solution.findMaxDuplicateNumbers((new int[]{}));
+    } catch (IllegalArgumentException e) {
+      System.out.println(e);
+    }
   }
 }
--- src/other/maxconsecutivenumbers/Solution.java
+++ src/other/maxconsecutivenumbers/Solution.java
@@ -2,28 +2,29 @@ package other.maxconsecutivenumbers;
 
 public class Solution {
   public static int[] findMaxDuplicateNumbers(int[] input) {
-    if(input.length == 0) {
-      return new int[]{0, 0};
+    if (input.length == 0) {
+      throw new IllegalArgumentException("Input cannot be empty");
     }
 
-    int resultCounter = 1;
-    int resultNumber = input[0];
-    int counter = 0;
+    int returnCount = 0;
+    int returnNumber = 0;
+    int count = 0;
     int number = 0;
 
     for(int i = 0; i < input.length; i++) {
-      if (number == input[i]) {
-        counter++;
+      int currentNumber = input[i];
+      if (currentNumber == number) {
+        count++;
       } else {
-        if (counter > resultCounter) {
-          resultCounter = counter;
-          resultNumber = number;
-        }
-        counter = 1;
-        number = input[i];
+        number = currentNumber;
+        count = 1;
+      }
+      if (count > returnCount) {
+        returnNumber = number;
+        returnCount = count;
       }
     }
 
-    return new int[]{resultNumber, resultCounter};
+    return new int[]{returnNumber, returnCount};
   }
 }
